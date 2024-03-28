08fc7b59f6e79bf12cb6f782e62445d4c45a2e4b
Patrick Drechsler <patrick@pdrechsler.de>
2024-03-28 22:49:04 +0100

    🎉 initial commit

---
+++ ToDoList/DataModel/ToDoItem.cs
@@ -0,0 +1,7 @@
+namespace ToDoList.DataModel;
+
+public class ToDoItem
+{
+    public string Description { get; set; } = string.Empty;
+    public bool IsChecked { get; set; }
+}
---
+++ ToDoList/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.Reactive.Linq;
+using ReactiveUI;
+using ToDoList.DataModel;
+using ToDoList.Services;
+
+namespace ToDoList.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    private ViewModelBase _contentViewModel;
+
+    public MainWindowViewModel()
+    {
+        var services = new ToDoListServices();
+        ToDoList = new ToDoListViewModel(services.GetItems());
+        _contentViewModel = ToDoList;
+    }
+
+    public ViewModelBase ContentViewModel
+    {
+        get => _contentViewModel;
+        private set => this.RaiseAndSetIfChanged(ref _contentViewModel, value);
+    }
+
+    public ToDoListViewModel ToDoList { get; }
+    
+    public void AddItem()
+    {
+        AddItemViewModel addItemViewModel = new();
+        //
+        // Observable.Merge(
+        //         addItemViewModel.OkCommand,
+        //         addItemViewModel.CancelCommand.Select(_ => (ToDoItem?)null))
+        //     .Take(1)
+        //     .Subscribe(newItem =>
+        //     {
+        //         if (newItem != null)
+        //         {
+        //             ToDoList.ListItems.Add(newItem);
+        //         }
+        //
+        //         ContentViewModel = ToDoList;
+        //     });
+
+        ContentViewModel = addItemViewModel;
+    }
+}
